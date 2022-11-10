c343e560803444110047b53801d09c6dc27c3252
JuanSanchezKZ <juanzete@gmail.com>
2022-11-10 20:43:19 -0300

    last commit

---
+++ AxelWebApi/Models/Restaurant.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AxelWebApi.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+
+        
+        public string Name { get; set; }
+        
+        public string Address { get; set; }
+        
+        public string Products { get; set; }
+        
+        public int Rating { get; set; }
+
+        [NotMapped]
+
+        public IFormFile Image { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public ICollection<Menu> Menus { get; set; }
+
+    }
+}
